b3658ea27a16ad495533d08aa4596a3ee376520c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-06-27 00:31:32 +0700

    fix: fix font bundling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import type { LayoutProps } from './$types';
+    import '@fontsource/inter-tight';
+    import '@fontsource/fira-sans';
     import './+layout.css';
 
     const { children }: LayoutProps = $props();
