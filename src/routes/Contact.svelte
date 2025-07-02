276a0673aa05d37a30f6d529737bc26cc7ecfdfe
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-07-02 14:36:35 +0700

    style(contact): adjust style

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -14,53 +14,75 @@
     </p>
 
     <ul class="w-fit mx-auto mt-8 space-y-2">
-        <li class="flex items-center gap-4">
-            <Mail class="text-base-fg-muted" />
-            <a href="mailto:duydang2412@gmail.com" class="group c-link">
-                duydang2412@gmail.com
-                <ExternalLink
-                    class="transition duration-150 inline opacity-0 group-hover:opacity-100"
-                />
+        <li>
+            <a
+                href="mailto:duydang2412@gmail.com"
+                class="relative group c-link inline-flex items-center gap-4 not-hover:not-active:text-base-fg"
+            >
+                <Mail />
+                <span>
+                    duydang2412@gmail.com
+                    <ExternalLink
+                        class="transition duration-150 text-link absolute -right-2 translate-x-full top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100"
+                    />
+                </span>
             </a>
         </li>
-        <li class="flex items-center gap-4">
-            <Discord class="size-6 fill-base-fg-muted" />
+        <li>
+            <a
+                href="https://github.com/duydang2311"
+                target="_blank"
+                class="relative group c-link inline-flex items-center gap-4 not-hover:not-active:text-base-fg"
+            >
+                <GitHub fill="currentColor" class="size-6 !invert-0" />
+                <div>
+                    duydang2311
+                    <ExternalLink
+                        class="transition duration-150 text-link absolute -right-2 translate-x-full top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100"
+                    />
+                </div>
+            </a>
+        </li>
+        <li>
+            <a
+                href="https://soundcloud.com/duydang12"
+                target="_blank"
+                class="relative group c-link inline-flex items-center gap-4 not-hover:not-active:text-base-fg"
+            >
+                <SoundCloud fill="currentColor" class="size-6 !invert-0" />
+                <div>
+                    duydang12
+                    <ExternalLink
+                        class="transition duration-150 text-link absolute -right-2 translate-x-full top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100"
+                    />
+                </div>
+            </a>
+        </li>
+        <li>
             <Clipboard props={{ value: 'duydang' }}>
                 {#snippet children(api)}
-                    <button {...api.getTriggerProps()} class="c-button group relative">
-                        <span>duydang</span>
-                        <span
-                            data-copied={api.copied ? '' : undefined}
-                            class="transition absolute -right-2 translate-x-full top-1/2 -translate-y-1/2 text-nowrap text-sm text-base-fg-muted opacity-0 group-hover:opacity-100 data-[copied]:opacity-100"
-                        >
-                            {#if api.copied}
-                                <ClipboardCheck />
-                            {:else}
-                            Click to copy
-                                <ClipboardIcon class="inline" />
-                            {/if}
-                        </span>
+                    <button
+                        {...api.getTriggerProps()}
+                        class="c-button group relative flex items-center gap-4"
+                    >
+                        <Discord fill="currentColor" class="size-6 !invert-0" />
+                        <div>
+                            <span>duydang</span>
+                            <span
+                                data-copied={api.copied ? '' : undefined}
+                                class="transition absolute -right-2 translate-x-full top-1/2 -translate-y-1/2 text-nowrap text-sm text-base-fg-muted opacity-0 group-hover:opacity-100 data-[copied]:opacity-100"
+                            >
+                                {#if api.copied}
+                                    <ClipboardCheck />
+                                {:else}
+                                    Click to copy
+                                    <ClipboardIcon class="inline" />
+                                {/if}
+                            </span>
+                        </div>
                     </button>
                 {/snippet}
             </Clipboard>
-        </li>
-        <li class="flex items-center gap-4">
-            <GitHub class="size-6 fill-base-fg-muted" />
-            <a href="https://github.com/duydang2311" target="_blank" class="group c-link">
-                duydang2311
-                <ExternalLink
-                    class="transition duration-150 inline opacity-0 group-hover:opacity-100"
-                />
-            </a>
-        </li>
-        <li class="flex items-center gap-4">
-            <SoundCloud class="size-6 fill-base-fg-muted" />
-            <a href="https://soundcloud.com/duydang12" target="_blank" class="group c-link">
-                duydang12
-                <ExternalLink
-                    class="transition duration-150 inline opacity-0 group-hover:opacity-100"
-                />
-            </a>
         </li>
     </ul>
 </Section>
