0585e3698c0bc97beaacf763b959bd4a3d1c00e2
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-07-02 14:07:22 +0700

    fix: height constraint works weirdly on safari

--- src/lib/components/Section.svelte
+++ src/lib/components/Section.svelte
@@ -4,6 +4,6 @@
     const { class: cls, children, ...props }: HTMLAttributes<HTMLElement> = $props();
 </script>
 
-<section {...props} class="h-screen min-h-max max-w-full py-20 px-4 {cls}">
+<section {...props} class="min-h-screen py-20 px-4 {cls}">
     {@render children?.()}
 </section>
